e2bc6dd38304a6cd4109f90265466b46182928e7
GeoXHub <frederik.schultz@business-science.dk>
2023-02-15 17:24:38 +0100

    Update logic and calcuations

--- sampcalc.js
+++ sampcalc.js
@@ -1,11 +1,15 @@
 var do_calcul = function() {
+    var mod = document.getElementById("buyingmodel");
+    var selected_mod = mod.options[mod.selectedIndex].text;
     var num1 = document.getElementById("BCR").value;
     var num2 = document.getElementById("CPM").value;
     var freq = document.getElementById("Frequency").value;
+    var ctr = document.getElementById("CTR").value;
     var maxbud = document.getElementById("maxbudget").value;
     num1 = Number(num1.replace(",","."));
     num2 = Number(num2.replace(",","."));
     freq = Number(freq.replace(",","."));
+    ctr  = Number(ctr.replace(",","."));
     mdearr = [0.05, 0.06, 0.07, 0.08, 0.09,  0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.2, 0.25, 0.3, 0.35];
     num1/= 100;
     conv = [];
@@ -20,10 +24,15 @@ var do_calcul = function() {
     conv[i] = (1+mdearr[i])*num1;
     conv1[i] = (1-mdearr[i])*num1;
     a[i] = 1.96 * Math.sqrt(((2+mdearr[i])*(2-2*(num1)-num1*mdearr[i])/2));
-    b[i] = 0.842 * Math.sqrt(1-num1 + (1+mdearr[i])*(1-num1-num1*mdearr[i]));
+    b[i] = 0.842 * Math.sqrt(1-num1 + (1+mdearr[i])*(1-num1-num1*mdearr[i])); 
     sampleSize1[i] = Math.pow(a[i]+b[i], 2)/(Math.pow(mdearr[i],2)*num1);
-    Budgets[i] = (num2/1000)*sampleSize1[i];
-    cost[i] = Budgets[i]*freq*2;
+    if(selected_mod == "CPM"){
+      Budgets[i] = (num2/1000)*sampleSize1[i]; // for CPM
+      cost[i] = Budgets[i]*freq*2;
+    }else{
+      Budgets[i] = (num2)*sampleSize1[i]; // for CPC
+      cost[i] = Budgets[i]*ctr*2;
+    }
     }
     let Budget = Math.max(...cost.filter(num => num <= maxbud));
 
@@ -39,8 +48,13 @@ var do_calcul = function() {
       risk1 = "HIGH RISK of not getting significant results"
       document.getElementById('risk').style.color = "red"
     }else{
-      risk1 = "You need at least " + (Math.min.apply(Math, Budgets)*2*freq).toFixed(2) + " " + document.getElementById('currency').value + " to conduct a test with these values"
-      document.getElementById('risk').style.color = "red"
+      if (selected_mod == "CPM"){
+        risk1 = "You need at least " + (Math.min.apply(Math, Budgets)*2*freq).toFixed(2) + " " + document.getElementById('currency').value + " to conduct a test with these values"
+        document.getElementById('risk').style.color = "red"
+      }else{
+        risk1 = "You need at least " + (Math.min.apply(Math, Budgets)*2*ctr).toFixed(2) + " " + document.getElementById('currency').value + " to conduct a test with these values"
+        document.getElementById('risk').style.color = "red"
+      }
     }
     
 
